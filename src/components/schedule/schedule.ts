e22438ea6d4d2fe93194c1ce8282a4f5c56dda47
marvzhang <tikazyq@163.com>
2021-06-18 16:28:54 +0800

    added batch create for schedules

--- src/components/schedule/schedule.ts
+++ src/components/schedule/schedule.ts
@@ -3,14 +3,22 @@ import {Store} from 'vuex';
 import useForm from '@/components/form/form';
 import useScheduleService from '@/services/schedule/scheduleService';
 import {getDefaultFormComponentData} from '@/utils/form';
-import {FORM_FIELD_TYPE_INPUT, FORM_FIELD_TYPE_INPUT_TEXTAREA, FORM_FIELD_TYPE_TAG_INPUT} from '@/constants/form';
+import {
+  FORM_FIELD_TYPE_INPUT,
+  FORM_FIELD_TYPE_INPUT_WITH_BUTTON,
+  FORM_FIELD_TYPE_SELECT,
+  FORM_FIELD_TYPE_SWITCH,
+} from '@/constants/form';
 import {parseExpression} from 'cron-parser';
 import {getModeOptions} from '@/utils/task';
+import useSpider from '@/components/spider/spider';
+import {TASK_MODE_RANDOM} from '@/constants/task';
 
 // get new schedule
 export const getNewSchedule = (): Schedule => {
   return {
     enabled: true,
+    mode: TASK_MODE_RANDOM,
   };
 };
 
@@ -22,14 +30,18 @@ const useSchedule = (store: Store<RootStoreState>) => {
   const ns = 'schedule';
   const state = store.state[ns];
 
-  // all spider dict
-  const allSpiderDict = computed<Map<string, Spider>>(() => store.getters['spider/allDict']);
-
+  const {
+    allListSelectOptions: allSpiderListSelectOptions,
+    allDict: allSpiderDict,
+  } = useSpider(store);
   // form
   const form = computed<Schedule>(() => state.form);
 
   // options for default mode
   const modeOptions = getModeOptions();
+
+  // readonly form fields
+  const readonlyFormFields = computed<string[]>(() => state.readonlyFormFields);
 
   // batch form fields
   const batchFormFields = computed<FormTableField[]>(() => [
@@ -42,24 +54,58 @@ const useSchedule = (store: Store<RootStoreState>) => {
       required: true,
     },
     {
-      prop: 'tags',
-      label: 'Tags',
-      width: '200',
-      placeholder: 'Tags',
-      fieldType: FORM_FIELD_TYPE_TAG_INPUT,
+      prop: 'spider_id',
+      label: 'Spider',
+      width: '150',
+      placeholder: 'Spider',
+      fieldType: FORM_FIELD_TYPE_SELECT,
+      options: allSpiderListSelectOptions.value,
+      disabled: () => readonlyFormFields.value.includes('spider_id'),
+      required: true,
     },
     {
-      prop: 'description',
-      label: 'Description',
-      width: '800',
-      placeholder: 'Description',
-      fieldType: FORM_FIELD_TYPE_INPUT_TEXTAREA,
+      prop: 'cron',
+      label: 'Cron Expression',
+      width: '150',
+      fieldType: FORM_FIELD_TYPE_INPUT,
+      placeholder: 'Name',
+      required: true,
+    },
+    {
+      prop: 'cmd',
+      label: 'Execute Command',
+      width: '200',
+      placeholder: 'Execute Command',
+      fieldType: FORM_FIELD_TYPE_INPUT_WITH_BUTTON,
+    },
+    {
+      prop: 'param',
+      label: 'Param',
+      width: '200',
+      placeholder: 'Param',
+      fieldType: FORM_FIELD_TYPE_INPUT_WITH_BUTTON,
+    },
+    {
+      prop: 'mode',
+      label: 'Default Run Mode',
+      width: '200',
+      fieldType: FORM_FIELD_TYPE_SELECT,
+      options: modeOptions,
+      required: true,
+    },
+    {
+      prop: 'enabled',
+      label: 'Enabled',
+      width: '80',
+      fieldType: FORM_FIELD_TYPE_SWITCH,
+      required: true,
     },
   ]);
 
   // form rules
   const formRules = readonly<FormRules>({
     cron: {
+      trigger: 'blur',
       validator: ((_, value: string, callback) => {
         const invalidMessage = 'Invalid cron expression. [min] [hour] [day of month] [month] [day of week]';
         if (!value) return callback(invalidMessage);
